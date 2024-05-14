4b88a403ae23f5d6bb1477346f9a74d161cd6bda
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2024-05-13 19:52:51 -0700

    implemented base functionality

--- src/main/java/com/ryjtoh/ProductService/controller/ProductController.java
+++ src/main/java/com/ryjtoh/ProductService/controller/ProductController.java
@@ -1,9 +1,32 @@
 package com.ryjtoh.ProductService.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ryjtoh.ProductService.dto.ProductRequest;
+import com.ryjtoh.ProductService.dto.ProductResponse;
+import com.ryjtoh.ProductService.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    @NotNull
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
--- src/main/java/com/ryjtoh/ProductService/repository/ProductRepository.java
+++ src/main/java/com/ryjtoh/ProductService/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.ryjtoh.ProductService.repository;
 import com.ryjtoh.ProductService.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public class ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
